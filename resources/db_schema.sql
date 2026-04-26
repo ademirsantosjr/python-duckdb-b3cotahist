c94533aead169b9a435fd744f161e283f003666b
Ademir Santos <ademir.m.junior@gmail.com>
2026-04-26 12:12:10 -0300

    Add info comments to the db schema

--- resources/db_schema.sql
+++ resources/db_schema.sql
@@ -1,26 +1,26 @@
 -- Table to store the stock quotes data
 CREATE TABLE IF NOT EXISTS cotacoes (
-    datpre      DATE,
-    codbdi      VARCHAR,
-    codneg      VARCHAR,
-    tpmerc      VARCHAR,
-    nomres      VARCHAR,
+    datpre      DATE, -- Date of the quote
+    codbdi      VARCHAR, -- Classifies securities in the Daily Information Bulletin
+    codneg      VARCHAR, -- Security code (ticker)
+    tpmerc      VARCHAR, -- Market type
+    nomres      VARCHAR, -- Full name of the security
     especi      VARCHAR,
     prazot      VARCHAR,
     modref      VARCHAR,
-    preabe      DOUBLE,
-    premax      DOUBLE,
-    premin      DOUBLE,
-    premed      DOUBLE,
-    preult      DOUBLE,
-    preofc      DOUBLE,
-    preofv      DOUBLE,
-    totneg      INTEGER,
-    quatot      BIGINT,
-    voltot      DOUBLE,
-    preexe      DOUBLE,
-    indopc      VARCHAR,
-    datven      DATE,
+    preabe      DOUBLE, -- Opening price
+    premax      DOUBLE, -- Maximum price
+    premin      DOUBLE, -- Minimum price
+    premed      DOUBLE, -- Average price
+    preult      DOUBLE, -- Last price
+    preofc      DOUBLE, -- Best ask price
+    preofv      DOUBLE, -- Best bid price
+    totneg      INTEGER, -- Total number of trades
+    quatot      BIGINT, -- Total quantity of securities traded
+    voltot      DOUBLE, -- Total financial volume of trades
+    preexe      DOUBLE, -- Exercise price for options
+    indopc      VARCHAR, 
+    datven      DATE, -- Expiration date for options
     fatcot      INTEGER,
     ptoexe      DOUBLE,
     codisi      VARCHAR,
